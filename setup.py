65106610d4e7b2a28ad3955d72a3a6d928779fe7
Pauloledes <paul.ledesma@hotmail.fr>
2022-03-12 23:46:09 +0100

    new release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='MySuperDF',
-     version="0.007",
+     version="0.008",
      scripts=['main.py'] ,
      author="Paul Ledesma",
      author_email="paul.ledesma@hotmail.fr",
